fa81a577dda7c1abef206d86f3c09671d2a0d0f9
maksim <maksim.buyanov@gmail.com>
2023-12-10 14:52:42 +0300

    add static settings wifi page

--- src/static/wifi/main.js
+++ src/static/wifi/main.js
@@ -3,9 +3,12 @@ const tg = window.Telegram.WebApp
 const form = document.querySelector('form')
 
 tg.onEvent('mainButtonClicked', function () {
+  if (!form || !form.elements.login || !form.elements.password) return
+
   const login = form.elements.login.value
   const password = form.elements.password.value
   const queryId = tg.initDataUnsafe?.query_id
   const data = JSON.stringify({ login, password, queryId })
+
   tg.sendData(data)
 })
