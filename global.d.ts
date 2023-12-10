64748d2275bad186cf4b304ff88fc3db284d8bf5
maksim <maksim.buyanov@gmail.com>
2023-12-10 14:52:18 +0300

    add global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare var process: {
+  env: {
+    CRYPTO_KEY: string
+    SERVER_PORT: string
+    DB_ADDRESS: string
+    DB_DIALECT: string
+    DB_BASE: string
+    DB_LOGIN: string
+    DB_PASS: string
+  }
+}
