459e54910917ac4c30e4198eaec84e0cd09918b8
maksim <maksim.buyanov@gmail.com>
2023-12-10 14:57:28 +0300

    add wifi settings

--- src/wifiSettings/wifiSettings.ts
+++ src/wifiSettings/wifiSettings.ts
@@ -1,4 +1,4 @@
 import { JsonSlice } from '../utils/json.js'
 import { resolve } from 'path'
 
-export const wifi = new JsonSlice(resolve('server', 'wifiSettings.json'))
+export const wifi = new JsonSlice(resolve(__dirname, 'wifiSettings.json'))
